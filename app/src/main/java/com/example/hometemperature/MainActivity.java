d76b7bd6a4c360449a2f2a3008e18155601fbfcc
Szymon Ozog <szymon.ozog@gmail.com>
2020-09-18 16:25:08 +0200

    Added ClientListen class

--- app/src/main/java/com/example/hometemperature/MainActivity.java
+++ app/src/main/java/com/example/hometemperature/MainActivity.java
@@ -12,12 +12,15 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.SocketException;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView temperatureTV;
     Button checkTemperatureBtn;
+    public static int port = 2390;
+    public static String ipAddress = "192.168.0.13";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                new Thread(new ClientSend()).start();
+               ClientListen listener = new ClientListen();
+               new Thread(listener).start();
             }
         });
     }
@@ -38,15 +43,41 @@ class ClientSend implements Runnable{
     @Override
     public void run(){
         try {
-            DatagramSocket udpSocket = new DatagramSocket(2390);
-            InetAddress serverAddress = InetAddress.getByName("192.168.0.13");
+            DatagramSocket udpSocket = new DatagramSocket(null);
+            udpSocket.setReuseAddress(true);
+            udpSocket.bind(new InetSocketAddress(MainActivity.port));
+            InetAddress serverAddress = InetAddress.getByName(MainActivity.ipAddress);
             byte[] buf = ("The String to Send").getBytes();
-            DatagramPacket packet = new DatagramPacket(buf, buf.length,serverAddress, 2390);
+            DatagramPacket packet = new DatagramPacket(buf, buf.length,serverAddress, MainActivity.port);
             udpSocket.send(packet);
         } catch (SocketException e) {
             Log.e("Udp:", "Socket Error:", e);
         } catch (IOException e) {
             Log.e("Udp Send:", "IO Error:", e);
+        }
+    }
+}
+
+class ClientListen implements  Runnable{
+
+    String temperature;
+    @Override
+    public void run(){
+        while(true){
+            try{
+                DatagramSocket udpSocket = new DatagramSocket(null);
+                udpSocket.setReuseAddress(true);
+                udpSocket.bind(new InetSocketAddress(MainActivity.port));
+                byte[] message = new byte[8000];
+                DatagramPacket packet = new DatagramPacket(message,message.length);
+                Log.i("UDP client: ", "about to wait to receive");
+                udpSocket.receive(packet);
+                temperature = new String(message, 0, packet.getLength());
+                Log.d("Received data", temperature);
+            }catch (IOException e) {
+            Log.e("Client has IOException", "error: ", e);
+            break;
+            }
         }
     }
 }
